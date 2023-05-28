f8ea3b24a204148ec1a290b0fc465ffe2866dcc9
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2023-05-28 21:03:58 +0530

    n=int(input()) l=list(map(int,input().split())) k=int(input()) p=[] c=0 def is_prime(x):     if x==0 or x==1:         return 0     for j in range(2,x//2+1):         if x%j==0:             return 0     else:         return 1 for i in range(n):     if is_prime(l[i])==1:         p.append(l[i]) for i in p:     if i>=k:         c+=1 print(c)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,20 @@
+n=int(input())
+l=list(map(int,input().split()))
+k=int(input())
+p=[]
+c=0
+def is_prime(x):
+    if x==0 or x==1:
+        return 0
+    for j in range(2,x//2+1):
+        if x%j==0:
+            return 0
+    else:
+        return 1
+for i in range(n):
+    if is_prime(l[i])==1:
+        p.append(l[i])
+for i in p:
+    if i>=k:
+        c+=1
+print(c)
