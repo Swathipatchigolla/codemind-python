94e400f5ff7c979d95446044bff65675fa2f27fb
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2023-05-28 21:13:58 +0530

    n=int(input()) l=list(map(int,input().split())) p=[] def is_prime(x):     if x==0 or x==1:         return 0     for j in range(2,x//2+1):         if x%j==0:             return 0     else:         return 1 for i in range(n):     if is_prime(l[i])==1:         p.append(l[i]) print(len(p))

---
+++ Primes_in_the_array.py
@@ -0,0 +1,15 @@
+n=int(input())
+l=list(map(int,input().split()))
+p=[]
+def is_prime(x):
+    if x==0 or x==1:
+        return 0
+    for j in range(2,x//2+1):
+        if x%j==0:
+            return 0
+    else:
+        return 1
+for i in range(n):
+    if is_prime(l[i])==1:
+        p.append(l[i])
+print(len(p))
