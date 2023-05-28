5176f9c8cca29ee2db4d27898aea338317fa8ada
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2023-05-28 18:22:15 +0530

    e=[] o=[] n=int(input()) l=list(map(int,input().split())) for i in l:     if i%2==0:         e.append(i)     else:         o.append(i) for i in e:     print(i,end=' ') for i in o:     print(i,end=' ')

---
+++ Even_elements_followed_by_odd_elements.py
@@ -0,0 +1,13 @@
+e=[]
+o=[]
+n=int(input())
+l=list(map(int,input().split()))
+for i in l:
+    if i%2==0:
+        e.append(i)
+    else:
+        o.append(i)
+for i in e:
+    print(i,end=' ')
+for i in o:
+    print(i,end=' ')
