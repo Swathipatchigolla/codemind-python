7d0488692e560331479c6a8676048657880b0246
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2023-05-29 16:20:02 +0530

    n=int(input()) l=list(map(int,input().split())) r=[] def add(x):     s=0     while x>0:         r=x%10         s+=1         x//=10     return s for i in l:     r.append(add(i)) m=min(r) print(r.count(m))

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+r=[]
+def add(x):
+    s=0
+    while x>0:
+        r=x%10
+        s+=1
+        x//=10
+    return s
+for i in l:
+    r.append(add(i))
+m=min(r)
+print(r.count(m))
