41c9b27022e03e063fcc95d22eedad6e29c20f66
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2023-06-03 10:33:45 +0530

    s=input() c=0 for i in s:     if i.isalpha():         pass     elif i.isdigit():         pass     else:         if i==' ':             pass         else:             c+=1 print(c)

---
+++ special_characters_count.py
@@ -0,0 +1,13 @@
+s=input()
+c=0
+for i in s:
+    if i.isalpha():
+        pass
+    elif i.isdigit():
+        pass
+    else:
+        if i==' ':
+            pass
+        else:
+            c+=1
+print(c)
