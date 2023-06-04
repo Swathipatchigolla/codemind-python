c959c63044419941ff613db5d4500e3320354b1c
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2023-06-04 15:37:29 +0530

    n=int(input()) for i in range(1,n+1):     x=1     r=2*i-1     for j in range(n-i):         print(' ',end='')     for k in range(r//2+1):         print(x,end='')         x+=1     x-=1     for l in range(r//2):         x-=1         print(x,end='')     print()

--- Pattern.py
+++ Pattern.py
@@ -1,5 +1,14 @@
 n=int(input())
 for i in range(1,n+1):
-    for j in range(n):
-        print(i,end=' ')
+    x=1
+    r=2*i-1
+    for j in range(n-i):
+        print(' ',end='')
+    for k in range(r//2+1):
+        print(x,end='')
+        x+=1
+    x-=1
+    for l in range(r//2):
+        x-=1
+        print(x,end='')
     print()
