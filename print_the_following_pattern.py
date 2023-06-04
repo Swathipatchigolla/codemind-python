5970dced61a0b9f1a61481455211fe5ee2fd4a56
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2023-06-04 15:10:58 +0530

    n=int(input()) for i in range(1,n+1):     for q in range(1,n-i+1):         print(' ',end='')     for j in range(1,n+1):         if j==1 or j==n or i==1 or i==n:             print('*',end='')         else:             print(' ',end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,10 @@
 n=int(input())
-for i in range(n):
-    for j in range(n):
-        if i==j or i+j==n-1:
-            print('x',end='')
+for i in range(1,n+1):
+    for q in range(1,n-i+1):
+        print(' ',end='')
+    for j in range(1,n+1):
+        if j==1 or j==n or i==1 or i==n:
+            print('*',end='')
         else:
-            print('0',end='')
+            print(' ',end='')
     print()
