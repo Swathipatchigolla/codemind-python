bd99875702f0eac768deedefdeca7755e4c078d4
Swathipatchigolla <132642626+Swathipatchigolla@users.noreply.github.com>
2023-06-04 14:37:33 +0530

    n=int(input()) x=ord('A') for i in range(n):     for j in range(n):         print(chr(x),end=' ')     x+=1     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,7 @@
 n=int(input())
-for i in range(1,n+1):
-    for j in range(n-i):
-        print(' ',end='')
-    for k in range(2*i-1):
-        print(i,end='')
+x=ord('A')
+for i in range(n):
+    for j in range(n):
+        print(chr(x),end=' ')
+    x+=1
     print()
